c63003a6c9a95836d77d1c96b45f7b98b08de302
jay sung <namic123@naver.com>
2023-09-22 09:11:02 +0900

    DTO, Java Beans

---
+++ src/main/java/com/example/spring20230920/domain/MyDto5.java
@@ -0,0 +1,14 @@
+package com.example.spring20230920.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+@NoArgsConstructor
+@RequiredArgsConstructor
+@AllArgsConstructor
+public class MyDto5 {
+    private String name;
+    private Integer age;
+
+}
