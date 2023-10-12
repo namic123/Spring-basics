7581017eb677c918577a58e30a03f1f54142733a
jay sung <namic123@naver.com>
2023-10-12 16:49:52 +0900

    기본키

---
+++ sql/25primarykey.sql
@@ -0,0 +1,38 @@
+-- PRIMARY KEY(주키, 기본키, PK)
+CREATE TABLE my_table27(
+    id INT PRIMARY KEY,
+    name VARCHAR(20)
+);
+
+CREATE TABLE my_table28(
+    id INT,
+    name VARCHAR(10),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE my_table29(
+    name VARCHAR(10),
+    birth DATE,
+    PRIMARY KEY (name,birth)
+);
+
+CREATE TABLE my_table30(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(10)
+);
+
+INSERT INTO my_table30 (name)
+VALUE ('손');
+
+
+INSERT INTO my_table30 (name)
+    VALUE ('lee');
+
+
+INSERT INTO my_table30 (name)
+    VALUE ('kim');
+
+SELECT * FROM my_table30;
+
+DELETE FROM my_table30
+WHERE id = 3;
