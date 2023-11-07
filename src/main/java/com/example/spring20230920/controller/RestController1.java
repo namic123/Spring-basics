5685cd0408715cf88bf9855c42e4d4a295d7b667
jay sung <namic123@naver.com>
2023-11-07 16:26:50 +0900

    React, axios.get 요청을 받아서 dao의 레코드 출력 해주는 예제

--- src/main/java/com/example/spring20230920/controller/RestController1.java
+++ src/main/java/com/example/spring20230920/controller/RestController1.java
@@ -1,5 +1,7 @@
 package com.example.spring20230920.controller;
 
+import com.example.spring20230920.dao.MyDao10;
+import com.example.spring20230920.dao.MyDao9;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,11 +12,21 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequiredArgsConstructor
 @RequestMapping("api/main1")
 public class RestController1 {
-
+    private final MyDao10 dao;
     @GetMapping("sub1")
     @ResponseBody
     public String method1(){
 
-        return "hello boot app!!";
+        return "customer name!!";
+    }
+
+    // http://localhost:8080/api/main1/sub2
+    // get 요청시
+    // 5번 고객의 customerName 응답
+    @GetMapping("sub2")
+    @ResponseBody
+    public String method2(){
+       String name = dao.customerName5();
+        return name;
     }
 }
