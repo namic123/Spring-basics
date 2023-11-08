d56d4379363b589089f61b185b967f0e7ecbd41f
jay sung <namic123@naver.com>
2023-11-08 11:35:31 +0900

    useEffect 활용, 프론트에서 id로 서버에 요청을 보내고 백에서 직원 엔티티를 응답해주는 예제

--- src/main/java/com/example/spring20230920/controller/RestController1.java
+++ src/main/java/com/example/spring20230920/controller/RestController1.java
@@ -2,8 +2,10 @@ package com.example.spring20230920.controller;
 
 import com.example.spring20230920.dao.MyDao10;
 import com.example.spring20230920.dao.MyDao9;
+import com.example.spring20230920.domain.MyDto33Employee;
 import com.example.spring20230920.domain.MyDto34Customer;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,4 +37,19 @@ public class RestController1 {
         public MyDto34Customer method3(){
         return dao.getCustomer();
     }
+    @GetMapping("sub4")
+    @ResponseBody
+    public ResponseEntity<MyDto34Customer> method4(MyDto34Customer dto){
+        MyDto34Customer customer= dao.getCustomerById(dto.getId());
+        if(customer == null) return ResponseEntity.notFound().build();
+        return ResponseEntity.ok(customer);
+    }
+    @GetMapping("sub5")
+    @ResponseBody
+    public ResponseEntity<MyDto33Employee> method5(MyDto33Employee dto){
+        MyDto33Employee employee = dao.getEmployee(dto.getId());
+        if(employee == null) return ResponseEntity.notFound().build();
+        return ResponseEntity.ok(employee);
+    }
+
 }
--- src/main/java/com/example/spring20230920/dao/MyDao10.java
+++ src/main/java/com/example/spring20230920/dao/MyDao10.java
@@ -1,5 +1,6 @@
 package com.example.spring20230920.dao;
 
+import com.example.spring20230920.domain.MyDto33Employee;
 import com.example.spring20230920.domain.MyDto34Customer;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
@@ -24,4 +25,23 @@ public interface MyDao10 {
                 WHERE CustomerID =5;
             """)
     MyDto34Customer getCustomer();
+
+    @Select("""
+                SELECT CustomerID id,
+                    CustomerName name,
+                    ContactName,
+                    Address,
+                    city,
+                     PostalCode,
+                     Country
+                FROM w3schools.customers
+                WHERE CustomerID = #{id};
+            """)
+    MyDto34Customer getCustomerById(Integer id);
+    @Select("""
+    SELECT lastName, firstName, BirthDate
+    FROM w3schools.employees
+    WHERE EmployeeID = #{id}
+""")
+    MyDto33Employee getEmployee(Integer id);
 }
